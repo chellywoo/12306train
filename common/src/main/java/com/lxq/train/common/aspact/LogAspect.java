86e004e7df7ada2fb8a91c337aa77ae269d94453
chellywoo <lxq0105s@163.com>
2024-03-05 20:17:23 +0800

    增加公共模块common，用于放置公共的类，比如工具类、拦截器、AOP、常量、枚举、公共配置等 增加公共配置，放入resources/Config包中，优先级高于resources包下的配置文件

--- common/src/main/java/com/lxq/train/common/aspact/LogAspect.java
+++ common/src/main/java/com/lxq/train/common/aspact/LogAspect.java
@@ -1,4 +1,4 @@
-package com.lxq.train.member.aspect;
+package com.lxq.train.common.aspact;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -26,7 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class LogAspect {
     //打印日志
     public LogAspect() {
-        System.out.println("LogAspect");
+        System.out.println("Common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
