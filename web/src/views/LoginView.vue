879b1879bdfb613988d1b2ea8ba168af73e5ac0c
chellywoo <lxq0105s@163.com>
2024-03-10 15:15:56 +0800

    完成短信验证码发送及登录功能

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -40,6 +40,7 @@
 <script>
 import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
+import { notification } from 'ant-design-vue';
 export default defineComponent({
   name: "LoginView",
   setup() {
@@ -51,11 +52,33 @@ export default defineComponent({
 
     const sendCode = () => {
       axios.post("http://localhost:8000/member/member/sendCode", {"mobile" : loginForm.mobile})
-          .then(response => { console.log(response) });
+          .then(response => {
+            console.log(response);
+            let data = response.data;
+            if(data.success){
+              notification.success({description: "发送验证码成功"});
+              loginForm.code = "8888";
+            }else{
+              notification.error({description: data.message});
+            }
+            });
+    };
+
+    const login = () => {
+      axios.post("http://localhost:8000/member/member/login", loginForm)
+          .then(response => {
+            let data = response.data;
+            if(data.success){
+              notification.success({description: "登录成功"});
+            }else{
+              notification.error({description: data.message});
+            }
+            });
     };
     return {
       loginForm,
       sendCode,
+      login
     };
   },
 });
