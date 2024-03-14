89375e821998828eb2d6ad304c79e3cac65779da
chellywoo <lxq0105s@163.com>
2024-03-14 15:08:27 +0800

    增加乘车人添加接口

---
+++ member/src/main/java/com/lxq/train/member/service/PassengerService.java
@@ -0,0 +1,28 @@
+package com.lxq.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.lxq.train.common.util.SnowUtil;
+import com.lxq.train.member.domain.Passenger;
+import com.lxq.train.member.mapper.PassengerMapper;
+import com.lxq.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+
+@Service
+public class PassengerService {
+//    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
+    @Resource
+    private PassengerMapper passengerMapper;
+    public void save(PassengerSaveReq passengerSaveReq){
+        Date now = new Date();
+        Passenger passenger = BeanUtil.copyProperties(passengerSaveReq, Passenger.class);
+        passenger.setId(SnowUtil.getSnowFlakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
