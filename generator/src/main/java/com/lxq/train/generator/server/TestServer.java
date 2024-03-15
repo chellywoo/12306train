78e5a1fe9bb68397427ab5b6287edfecc944c120
chellywoo <lxq0105s@163.com>
2024-03-15 19:32:41 +0800

    集成tom4j，读取当前持久层xml，得到表名和实体名

--- generator/src/main/java/com/lxq/train/generator/server/TestServer.java
+++ generator/src/main/java/com/lxq/train/generator/server/TestServer.java
@@ -1,6 +1,7 @@
 package com.lxq.train.generator.server;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -16,6 +17,18 @@ public class TestServer {
     }
 
     public static void main(String[] args) throws Exception {
+        String generatorPath = generatorPath();
+
+        Document document = new SAXReader().read("generator/"+ generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println("table = " + table);
+        Node tableName = table.selectSingleNode("@tableName");
+        System.out.println("tableName = " + tableName.getText());
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println("domainObjectName = " + domainObjectName.getText());
+    }
+
+    private static String generatorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -23,5 +36,6 @@ public class TestServer {
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
+        return node.getText();
     }
 }
