9b46f06b357f3bca693149605c49e8b89c4205db
chellywoo <lxq0105s@163.com>
2024-03-17 12:05:56 +0800

    增加DbUtil,Field，获取表中所有字段的信息

--- generator/src/main/java/com/lxq/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/lxq/train/generator/server/ServerGenerator.java
@@ -1,5 +1,7 @@
 package com.lxq.train.generator.server;
 
+import com.lxq.train.generator.util.DbUtil;
+import com.lxq.train.generator.util.Field;
 import com.lxq.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -10,6 +12,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -34,10 +37,26 @@ public class ServerGenerator {
         System.out.println("tableName = " + tableName.getText());
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println("domainObjectName = " + domainObjectName.getText());
-        
+
+        Node connectUrl = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("connectUrl = " + connectUrl.getText());
+        System.out.println("userId = " + userId.getText());
+        System.out.println("password = " + password.getText());
+
+        DbUtil.url = connectUrl.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
+//        DbUtil.getConnection();
+
         String Domain = domainObjectName.getText();
         String domain = Domain.substring(0,1).toLowerCase() + Domain.substring(1);
-        
+
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+
         Map<String, Object> param = new HashMap<>();
         param.put("Domain",Domain);
         param.put("domain",domain);
