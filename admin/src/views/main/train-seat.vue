7e1afcecdf451cc1fd8448d61106ab331fbfbb92
chellywoo <lxq0105s@163.com>
2024-03-18 21:05:41 +0800

    重新生成座位界面，使其变为只读

--- admin/src/views/main/train-seat.vue
+++ admin/src/views/main/train-seat.vue
@@ -4,7 +4,6 @@
     <a-space style="width: 100%">
       <train-select-view v-model="params.trainCode" width="200px"/>
       <a-button type="primary" @click="handleQuery()">查找</a-button>
-      <a-button type="primary" @click="OnAdd"><plus-outlined />新增</a-button>
     </a-space>
   </p>
   <a-table :dataSource="trainSeats"
@@ -14,13 +13,6 @@
            :loading="loading">
     <template #bodyCell="{ column,record }">
       <template v-if="column.key === 'operation'">
-        <a-space>
-          <a style="color: blueviolet" @click="OnEdit(record)"><edit-outlined/>编辑</a>
-          <a-popconfirm title="删除后不可恢复，确定删除?" ok-text="确认" cancel-text="取消" @confirm="OnDelete(record)">
-            <delete-outlined style="color: red"/>
-            <a style="color: red">删除</a>
-          </a-popconfirm>
-        </a-space>
       </template>
       <template v-else-if="column.key === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
@@ -38,40 +30,6 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="座位" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form
-        :model="trainSeat"
-        :label-col="{ span: 4 }"
-        :wrapper-col="{ span: 20 }">
-      <a-form-item label="车次编号">
-        <train-select-view v-model="trainSeat.trainCode"/>
-      </a-form-item>
-      <a-form-item label="厢序">
-        <a-input v-model:value="trainSeat.carriageIndex"/>
-      </a-form-item>
-      <a-form-item label="排号">
-        <a-input v-model:value="trainSeat.row"/>
-      </a-form-item>
-      <a-form-item label="列号">
-        <a-select v-model:value="trainSeat.col">
-          <a-select-option v-for="item in SEAT_COL_ARRAY" :key="item.code" :value="item.code">
-            {{ item.desc }}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="座位类型">
-        <a-select v-model:value="trainSeat.seatType">
-          <a-select-option v-for="item in SEAT_TYPE_ARRAY" :key="item.code" :value="item.code">
-            {{ item.desc }}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="同车厢座序">
-        <a-input v-model:value="trainSeat.carriageSeatIndex"/>
-      </a-form-item>
-    </a-form>
-  </a-modal>
 </template>
 <script>
 import {defineComponent, onMounted, ref} from "vue";
@@ -129,11 +87,6 @@ export default defineComponent({
         dataIndex: 'carriageSeatIndex',
         key: 'carriageSeatIndex',
       },
-      {
-        title: "操作",
-        dataIndex: 'operation',
-        key: 'operation'
-      }
     ];
     const pagination = ref({
       total: 0,
@@ -146,46 +99,6 @@ export default defineComponent({
     let params = ref({
       trainCode: null
     })
-
-    const OnAdd = () => {
-      trainSeat.value = {};
-      visible.value = true;
-    };
-
-    const OnEdit = (record) => {
-      trainSeat.value = window.Tool.copy(record);
-      visible.value = true;
-    }
-
-    const OnDelete = (record) => {
-      axios.delete("/business/admin/train-seat/delete/" + record.id).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({description: "删除成功!"});
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize
-          })
-        } else {
-          notification.error({description: data.message});
-        }
-      })
-    }
-    const handleOk = () => {
-      axios.post("/business/admin/train-seat/save", trainSeat.value).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({description: "保存成功!"});
-          visible.value = false;
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize
-          })
-        } else {
-          notification.error({description: data.message});
-        }
-      })
-    };
 
     const handleQuery = (param) => {
       if (!param) {
@@ -240,10 +153,6 @@ export default defineComponent({
       loading,
       handleQuery,
       handleTableChange,
-      OnAdd,
-      OnEdit,
-      OnDelete,
-      handleOk,
       params
     };
   },
