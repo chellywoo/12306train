2e732c1aa5601edb556f19d9b85c805d548d740f
chellywoo <lxq0105s@163.com>
2024-03-19 20:03:03 +0800

    演示springboot分布式锁

---
+++ batch/src/main/java/com/lxq/train/batch/job/SpringbootBatchJob.java
@@ -0,0 +1,19 @@
+package com.lxq.train.batch.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/***
+ * - 只适合单体应用，集群时就不适合了，如果有多个节点同时跑批，这种情况只能增加分布式锁来解决，确保同一时刻只有一个节点进行跑批；
+ * - 没法实时更改定时任务状态和策略
+ */
+@Component
+@EnableScheduling
+public class SpringbootBatchJob {
+    @Scheduled(cron = "0/5 * * * * ?")
+    private void test(){
+        // 分布式锁来解决，确保同一时刻只有一个节点进行跑批
+        System.out.println("SpringbootBatchJob TEST");
+    }
+}
