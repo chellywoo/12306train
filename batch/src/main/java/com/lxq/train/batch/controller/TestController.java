0567019da05d5883ab6bf45ed9edd56ac75905df
chellywoo <lxq0105s@163.com>
2024-03-22 09:51:19 +0800

    集成openFeign,实现服务间调用，给每个应用增加应用名，可搭配配置中心使用

--- batch/src/main/java/com/lxq/train/batch/controller/TestController.java
+++ batch/src/main/java/com/lxq/train/batch/controller/TestController.java
@@ -1,12 +1,23 @@
 package com.lxq.train.batch.controller;
 
+import com.lxq.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello(){
+        String businessHello = businessFeign.Hello();
+        LOG.info(businessHello);
         return "hello world!!! Batch!";
     }
 }
