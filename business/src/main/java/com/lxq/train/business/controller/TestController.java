4fb4d486e14bda3ccccb10b067817ab69d4c3607
chellywoo <lxq0105s@163.com>
2024-04-09 21:01:51 +0800

    学习sentinel的流控模式

--- business/src/main/java/com/lxq/train/business/controller/TestController.java
+++ business/src/main/java/com/lxq/train/business/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.lxq.train.business.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.lxq.train.business.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Resource
+    TestService testService;
     @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() throws InterruptedException {
+        testService.hello2();
         return "hello world!!! Business!";
+    }
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException {
+//        Thread.sleep(500);
+        testService.hello2();
+        return "hello world!!! Business1!";
     }
 }
