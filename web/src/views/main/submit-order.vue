7d3d2050bec23e6b1630e7f5855b679d920406ad
chellywoo <lxq0105s@163.com>
2024-04-11 19:16:32 +0800

    增加第一层验证码削弱瞬时高峰，减少第二层验证码中后端接口的压力

--- web/src/views/main/submit-order.vue
+++ web/src/views/main/submit-order.vue
@@ -55,7 +55,7 @@
     <a-button type="primary" size="large" @click="finishCheckPassenger">提交订单</a-button>
   </div>
   <a-modal v-model:visible="visible" title="请核对信息" style="top: 50px; width: 800px" ok-text="确认"
-           cancel-text="取消" @ok="showImageCodeModal()">
+           cancel-text="取消" @ok="showFirstImageCodeModal()">
     <div class="order-tickets">
       <a-row class="order-tickets-header" v-if="tickets.length > 0">
         <a-col :span="5">乘客</a-col>
@@ -108,7 +108,7 @@
     {{ tickets }}
   </a-modal>
 
-  <!-- 验证码 -->
+  <!-- 第二层验证码 后端-->
   <a-modal v-model:visible="imageCodeModalVisible" :title="null" :footer="null" :closable="false"
            105 style="top: 50px; width: 400px">
     <p style="text-align: center; font-weight: bold; font-size: 18px">使用服务端验证码削弱瞬时高峰<br/>防止机器人刷票
@@ -121,6 +121,21 @@
       </a-input>
     </p>
     <a-button type="danger" block @click="handleOk">输入验证码后开始购票</a-button>
+  </a-modal>
+
+  <!-- 第一层验证码 纯前端 -->
+  <a-modal v-model:visible="firstImageCodeModalVisible" :title="null" :footer="null" :closable="false"
+           105 style="top: 50px; width: 400px">
+    <p style="text-align: center; font-weight: bold; font-size: 18px">使用纯前端验证码削弱瞬时高峰<br/>减少后端验证码接口压力
+    </p>
+    <p>
+      <a-input v-model:value="firstImageCodeTarget" placeholder="验证码">
+        <template #suffix>
+          {{ firstImageCodeSourceA }} + {{ firstImageCodeSourceB }}
+        </template>
+      </a-input>
+    </p>
+    <a-button type="danger" block @click="validFirstImageCode">提交验证码</a-button>
   </a-modal>
 </template>
 <script>
@@ -328,13 +343,15 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           notification.success({description: "下单成功！"});
+          imageCodeModalVisible.value = false;
+          visible.value = false;
         } else {
           notification.error({description: data.message});
         }
       });
     }
 
-    /*-----------------验证码------------------*/
+    /*-----------------第二层验证码------------------*/
     const imageCodeModalVisible = ref();
     const imageCodeToken = ref();
     const imageCodeSrc = ref();
@@ -349,6 +366,34 @@ export default defineComponent({
     const showImageCodeModal = () => {
       loadImageCode();
       imageCodeModalVisible.value = true;
+    };
+
+    /*-----------------第一层验证码------------------*/
+    const firstImageCodeModalVisible = ref();
+    const firstImageCodeSourceA = ref();
+    const firstImageCodeSourceB = ref();
+    const firstImageCodeTarget = ref();
+    /**
+     * 加载图形验证码
+     */
+    const loadFirstImageCode = () => {
+      firstImageCodeSourceA.value = Math.floor(Math.random() * 10 + 1) + 10;
+      console.log(firstImageCodeSourceA.value);
+      firstImageCodeSourceB.value = Math.floor(Math.random() * 10 + 1) + 20;
+      console.log(firstImageCodeSourceA.value);
+    };
+    const showFirstImageCodeModal = () => {
+      loadFirstImageCode();
+      firstImageCodeModalVisible.value = true;
+    };
+    const validFirstImageCode = () => {
+      if (parseInt(firstImageCodeTarget.value) === parseInt(firstImageCodeSourceA.value + firstImageCodeSourceB.value)) {
+        // 第一层验证通过
+        firstImageCodeModalVisible.value = false;
+        showImageCodeModal();
+      } else {
+        notification.error({description: '验证码错误'});
+      }
     };
 
     onMounted(() => {
@@ -375,7 +420,14 @@ export default defineComponent({
       imageCodeToken,
       imageCodeModalVisible,
       loadImageCode,
-      showImageCodeModal
+      showImageCodeModal,
+      firstImageCodeSourceA,
+      firstImageCodeSourceB,
+      firstImageCodeTarget,
+      firstImageCodeModalVisible,
+      loadFirstImageCode,
+      showFirstImageCodeModal,
+      validFirstImageCode
     };
   },
 });
